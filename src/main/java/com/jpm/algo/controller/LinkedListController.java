ec76989fd9bc7cc2cb5b6748334b7a42733ac90d
Mok <okmokok@naver.com>
2016-01-19 22:04:54 +0900

    [algo-04]linked list 추

---
+++ src/main/java/com/jpm/algo/controller/LinkedListController.java
@@ -0,0 +1,29 @@
+package com.jpm.algo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.jpm.algo.model.LinkedList;
+ 
+// https://opentutorials.org/module/1335/8857
+@Controller
+public class LinkedListController {
+	@RequestMapping(value = {"/04"}, method = RequestMethod.GET)
+	public String findAllBinaryString() {
+		LinkedList linkedList = new LinkedList();
+		
+		linkedList.addFirst(30);
+		linkedList.addFirst(20);
+		linkedList.addFirst(10);
+		
+		linkedList.add(1, 15);
+		
+		for (int i=0; i<linkedList.getSize(); i++) {
+			System.out.println(linkedList.getNode(i)); 
+		}
+	
+		
+		return "home";
+	}
+}
---
+++ src/main/java/com/jpm/algo/model/LinkedList.java
@@ -0,0 +1,127 @@
+package com.jpm.algo.model;
+
+public class LinkedList {
+	private Node head;
+	private Node tail;
+	private int size = 0;
+	
+	public LinkedList() {
+		
+	}
+	
+	
+	private class Node<T> {
+		T data;
+		Node next;
+		
+		public Node(T data) {
+			this.data = data;
+			this.next = null;
+		}
+		
+		public T getData() {
+			return this.data;
+		}
+		
+
+		
+		public String toString() {
+			return String.valueOf(data);
+		}
+	}
+	
+	public int getSize() {
+		return this.size;
+	}	
+
+	public void addFirst(Object data) {
+		Node node = new Node(data);
+		node.next = this.head;
+		this.head = node;
+		this.size++;
+		
+		if(node.next == null) {
+			this.tail = head;
+		}
+	}
+	
+	public void addLast(Object data) {
+		Node node = new Node(data);
+		
+		if (size == 0) {
+			addFirst(data);
+		} else {
+			tail.next = node;
+			node = tail;
+			
+			size++;
+			
+		}
+	}
+	
+	public Node getNode(int index) {
+		Node node = head;
+		
+		for (int i=0; i<index; i++) {
+			node = node.next;
+		}
+		
+		return node;
+	}
+	
+	public Object getData(int index) {
+		return this.getNode(index).getData();
+	}
+	
+	public void add(int index , Object object) {
+		if (index == 0) {
+			addFirst(object);
+		} else {
+			Node<Object> temp1 = this.getNode(index - 1);
+			
+			Node<Object> temp2 = temp1.next;
+			
+			Node<Object> node = new Node(object);
+			
+			temp1.next = node;
+			
+			node.next = temp2;
+			
+			size++;
+			
+			if (node.next == null) {
+				tail = node;
+			}
+			
+		}		
+	}
+	
+	public void removeFirst() {
+		Node temp = head;
+		head = temp.next;
+		size--;
+	}
+	
+	public void remove(int index) {
+		if (index == 0) {
+			removeFirst();
+		} else {
+			Node temp = this.getNode(index - 1);
+			Node removeNode = temp.next;
+			
+			temp.next = temp.next.next;
+			
+			if (removeNode == tail) {
+				tail = temp;
+			}
+			
+			removeNode = null;
+			
+			size--;
+			
+			
+		}
+	}
+	
+	
+}
