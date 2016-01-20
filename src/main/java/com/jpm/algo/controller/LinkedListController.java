8bab0b7ecb37af7c4844f9b1296a9710a82c99f8
Mok <okmokok@naver.com>
2016-01-20 23:09:12 +0900

    test4

--- src/main/java/com/jpm/algo/controller/LinkedListController.java
+++ src/main/java/com/jpm/algo/controller/LinkedListController.java
@@ -42,6 +42,7 @@ public class LinkedListController {
 		System.out.println("test 1");
 		System.out.println("test 2");
 		System.out.println("test 3");
+		System.out.println("test 4");
 		
 		return "home";
 	}
