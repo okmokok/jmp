9efa09a531b9d1bc8d6eb8adfc7082deed174697
Mok <okmokok@naver.com>
2016-01-20 22:43:42 +0900

    새기능 추가2 삭

--- src/main/java/com/jpm/algo/controller/LinkedListController.java
+++ src/main/java/com/jpm/algo/controller/LinkedListController.java
@@ -38,10 +38,7 @@ public class LinkedListController {
 		System.out.println(linkedList.indexOf(10)); // 0
 		System.out.println(linkedList.indexOf(20)); // 1
 		System.out.println(linkedList.indexOf(30)); // 2
-		
-		System.out.println("새기능 추가 2");
 
-		
 		return "home";
 	}
 }
